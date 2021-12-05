1810c932e51cb972b686ddb72b88682c8e9387e5
Menor25 <ajirimenor@gmail.com>
2021-12-04 21:29:44 -1200

    monty header file created for the task

--- monty.h
+++ monty.h
@@ -16,21 +16,6 @@
 #define TRUE 1
 #define FALSE 0
 #define DELIMS "\n \t\r"
-/**
- * struct stack_s - doubly linked list representation of a stack (or queue)
- * @n: integer
- * @prev: points to the previous element of the stack (or queue)
- * @next: points to the next element of the stack (or queue)
- *
- * Description: doubly linked list node structure
- * for stack, queues, LIFO, FIFO Holberton project
- */
-typedef struct stack_s
-{
-	int n;
-	struct stack_s *prev;
-	struct stack_s *next;
-} stack_t;
 
 /**
  * struct instruction_s - opcoode and its function
@@ -42,56 +27,73 @@ typedef struct stack_s
  */
 typedef struct instruction_s
 {
-	char *opcode;
-	void (*f)(stack_t **stack, unsigned int line_number);
+    char *opcode;
+    void (*f)(stack_t **stack, unsigned int line_number);
 } instruction_t;
 
-/**
- * struct glob_s - globally useful variables, all rolled into one
- * @top: double pointer to top of stack
- * @ops: double pointer to an instruction struct
-**/
-typedef struct glob_s
+typedef struct stack_s
 {
-	stack_t **top;
-	instruction_t **ops;
-} glob_t;
+	int n;
+	struct stack_s *prev;
+	struct stack_s *next;
+} stack_t;
 
-extern glob_t glob;
+/* executer functions*/
+void open_and_read(char **argv);
+int is_number(char *token);
+int is_comment(char *token, int line_counter);
 
-/* monty.c */
-void stack_init(stack_t **head);
-void free_all(void);
 
-/* helper1.c */
-int process_file(char *filename, stack_t **stack);
+// char **parse(char *text);
+// void pall(stack_t *bottom);
+// stack_t *push(stack_t *bottom, unsigned int n);
 
-/* helper2.c */
-void delegate_op(stack_t **stack, char *op, unsigned int line_number);
+/*opcodes */
+void (*get_op_code(char *token, unsigned int line)) (stack_t **, unsigned int);
 
-/* instruction1.c */
-void instruction_push(stack_t **stack, unsigned int line_number);
-void instruction_pop(stack_t **stack, unsigned int line_number);
-void instruction_pint(stack_t **stack, unsigned int line_number);
-void instruction_pall(stack_t **stack, unsigned int line_number);
-void instruction_swap(stack_t **stack, unsigned int line_number);
+/* Stack */
+void push_stack(stack_t **top, unsigned int line_number);
+void pall_stack(stack_t **top, unsigned int line_number);
+void free_stack(stack_t *top);
+void pint_stack(stack_t **top, unsigned int line_number);
+void pop_stack(stack_t **top, unsigned int line_number);
 
-/* instruction2.c */
-void instruction_add(stack_t **stack, unsigned int line_number);
-void instruction_nop(stack_t **stack, unsigned int line_number);
-void instruction_sub(stack_t **stack, unsigned int line_number);
-void instruction_div(stack_t **stack, unsigned int line_number);
-void instruction_mul(stack_t **stack, unsigned int line_number);
+/* error_handler functions */
+void err_arguments(void);
+void open_err(char **);
+void invalid_err(char *invInstruction, unsigned int line);
+void not_int_err(unsigned int line);
+void malloc_err(void);
 
-/* instruction3.c */
-void instruction_mod(stack_t **stack, unsigned int line_number);
-void instruction_pchar(stack_t **stack, unsigned int line_number);
-void instruction_pstr(stack_t **stack, unsigned int line_number);
-void instruction_rotl(stack_t **stack, unsigned int line_number);
-void instruction_rotr(stack_t **stack, unsigned int line_number);
+/**
+ * Invalid Err -Main entry
+ *Description: print message if when trying to open the file we got invalid instruction
+ * @invError: instruction
+ * @line: line
+ * Return: void
+ **/
+void invalid_er(char *invError, unsigned int line)
+{
+	fprintf(stderr, "L%u: unknown instruction %s\n", line, invError);
+	exit(EXIT_FAILURE);
+}
 
-/* _strtol.c */
-int is_leading_digit(char ascii_char);
-int _strtol(char *num_string, unsigned int line_number);
+/**
+ * malloc_err -Main entry
+ *Description: print error message if malloc failed to allocate memory
+ * Return: void
+ **/
+void malloc_err(void)
+{
+	fprintf(stderr, "Error: malloc failed\n");
+	exit(EXIT_FAILURE);
+}
 
-#endif /* MONTY */
+/* Return error if there is no value in the stack */
+void pint_err(unsigned int line);
+void pop_err(unsigned int line);
+void swap_err(unsigned int line);
+void add_err(unsigned int line);
+void sub_err(unsigned int line);
+
+#endif /* MONTY_H */
